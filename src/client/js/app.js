b10a9ad71335fdcb9177fa70564c0e2681a17f5d
Sárpátki Ádám <sepa1989@gmail.com>
2020-05-02 20:41:39 +0200

    feat: add function to get date diff in days and weather on travel date

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,9 +4,10 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let locationInput = document.getElementById('destination-input').value
-    
-    fetch(`http://localhost:8081/locationInfo?location=${locationInput}`)
+    let locationInput = document.getElementById('destination-input').value;
+    let date = document.getElementById('date-input').value;
+    console.log(date);
+    fetch(`http://localhost:8081/locationInfo?location=${locationInput}&date=${date}`)
     .then(res => res.json())
     .then(function(res) {
         // document.getElementById('results').innerHTML = generateMessage(res);
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const Geonames = require('geonames.js');
+const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -32,17 +33,34 @@ app.listen(8081, function () {
   console.log('Example app listening on port 8081!')
 })
 
+// Function to get the difference between current date and the travel date
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+
 app.get('/locationInfo', async function (req, res) {
   // async/await
   try {
     const results = await geonames.search({ name_equals: req.query.location })
     const city = results.geonames[0];
-    fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${city.lat}&lon=${city.lng}&key=${process.env.weatherbitKEY}`)
-    .then(res => res.json())
-    .then(json => console.log(json));
+    const getWeatherRaw = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${city.lat}&lon=${city.lng}&key=${process.env.weatherbitKEY}`)
+    const getWeatherJson = await getWeatherRaw.json();
+    fs.writeFileSync('kacsa.txt', JSON.stringify(getWeatherJson, null, 2));
+    const a = new Date(),
+    b = new Date(req.query.date),
+    difference = dateDiffInDays(a, b);
+    console.log(getWeatherJson.data[difference]);
+
     
     
-    // res.send({lng: city.lng, lat: city.lat});
   } catch (err) {
     console.error(err);
   }
