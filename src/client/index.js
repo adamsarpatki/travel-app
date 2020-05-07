118e7c58c5c1cfd36673fa2b613dd181e368b71a
Sárpátki Ádám <sepa1989@gmail.com>
2020-05-07 16:12:10 +0200

    feat: add further css to project

--- src/client/index.js
+++ src/client/index.js
@@ -3,13 +3,15 @@ import { storeItem, deleteItem, getItem } from './js/localStorage.js'
 
 import './styles/resets.scss'
 import './styles/base.scss'
+import './styles/buttons.scss'
+import './styles/contents.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
 export { handleSubmit, storeItem, deleteItem, generateHTML, getItem }
 
-document.getElementById('button').addEventListener('click', handleSubmit);
+document.getElementById('submit-button').addEventListener('click', handleSubmit);
 
 document.addEventListener('DOMContentLoaded', function () {
     const trips = getItem()
